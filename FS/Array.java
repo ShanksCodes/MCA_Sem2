2e9858be6138047c03e251910ff80acc712b07a6
ShanksCodes <99173788+ShanksCodes@users.noreply.github.com>
2023-03-21 10:44:13 +0530

    added FS

---
+++ FS/Array.java
@@ -0,0 +1,13 @@
+public class Array {
+    private int[] Array;
+    private int count;
+    public Array(int item) {
+        Array = new int[item];
+    }
+    public void insert(int item){
+        for (int i = 0; i < count; i++) {
+            Array[i]= item ;
+        }
+        public void print()
+    }
+}
---
+++ FS/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String args[]){
+        Array numbers = new Array(5);
+        numbers.insert(98);
+        numbers.insert(18);
+        numbers.insert(78);
+        numbers.insert(88);
+        numbers.insert(68);
+    }
+}
