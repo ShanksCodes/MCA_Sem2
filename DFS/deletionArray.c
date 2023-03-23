a49a691f8bace9110d518730cfe4cfb994e1f87d
Abhishek Tyagi <abhishektyagiofficial99@gmail.com>
2023-03-23 10:42:54 +0530

    zzz

--- DFS/deletionArray.c
+++ DFS/deletionArray.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
-void insertInArray(int[],int&,int,int);
-
+void deletionInArray(int[],int*,int);
 
 int main() {
 
    int arr[15];
    int size;
-   int el;
    int pos;
    int i;
 
@@ -17,36 +15,32 @@ int main() {
    for(i=0;i<size;++i)
      scanf("%d",&arr[i]);
      
-   printf("Enter the element to insert: ");
-   scanf("%d",&el); 
-
-   printf("Enter the index to insert element at: ");
+   printf("\nEnter the index of element to be deleted from array: ");
    scanf("%d",&pos); 
- 
-   insertInArray(arr,size,el,pos);
 
-   printf("\nElement inserted successfully. New array:\n");
+   deletionInArray(arr,&size,pos);
+
+   printf("\nElement deleted successfully. New array:\n");
    for(i=0;i<size;i++)
     {
         printf("%d ",arr[i]);
     }
 
    
-
    return 0;
 }
 
 
-void insertInArray(int arr[], int &size, int el, int pos)
+
+void deletionInArray(int arr[], int *size, int pos)
  {
   
-    int i=0;
+     int i=0;
 
-    for(i=size; i>pos;--i)
-     {
-        arr[i] = arr[i-1];
-     }
+     for(i=pos; i<*(size-1);++i)
+      {
+        arr[i]=arr[i+1];
+      }
 
-    arr[pos]= el;
-    size++; 
+     --(*size); 
  }
