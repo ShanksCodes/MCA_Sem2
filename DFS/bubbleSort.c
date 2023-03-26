b89e8c1e238d04485eb13c39c0806073b6ac28fc
ShanksCodes <99173788+ShanksCodes@users.noreply.github.com>
2023-03-26 14:04:30 +0530

    bubble sort done

--- DFS/bubbleSort.c
+++ DFS/bubbleSort.c
@@ -9,6 +9,7 @@ int main() {
    int size;
    int key;
    int i;
+   printf("\n----------------------------------------------------------------------------\n");
 
    printf("Enter the size of the array(max 15): ");
    scanf("%d",&size);
@@ -18,7 +19,7 @@ int main() {
      
    bubbleSort(arr,size);
 
-   printf("\nElement inserted successfully. New array:\n");
+   printf("\nArray sorted successfully. New array:\n");
 
    for(i=0;i<size;i++)
      printf("%d ",arr[i]);
@@ -32,8 +33,21 @@ void bubbleSort(int arr[], int size)
   
     int i=0;
     int flag=0;
-    int j=0
+    int j=0;
+    int temp=0;
      
+     for(i=0;i<size-1;++i)
+      {
+        for(j=0;j<size-1-i;++j)
+         {
+           if(arr[j]>arr[j+1])
+            {
+              temp = arr[j];
+              arr[j]=arr[j+1];
+              arr[j+1]=temp;
+            }
+         }
+      }
 
  }
 
