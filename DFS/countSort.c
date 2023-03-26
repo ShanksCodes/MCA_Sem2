3b0b34f983981abdab833259bf27c035e286adeb
ShanksCodes <99173788+ShanksCodes@users.noreply.github.com>
2023-03-26 17:47:11 +0530

    added count sort

--- DFS/countSort.c
+++ DFS/countSort.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void countSort(int[],int size); //for only 1 occurence
 
@@ -33,21 +34,49 @@ void countSort(int arr[], int size)
     int i=0;
     int j=0; //
     int key=0;
+    int*count_arr;
+    int*output_arr;
      
-     for(i=1;i<size;++i)
+    int max=arr[0];
+    for(i=1; i<size; ++i)
+     {
+      if(arr[i]>max)
+       max=arr[i];
+     }
+     
+    count_arr = (int*)malloc((max+1)*sizeof(int)); 
+    output_arr = (int*)malloc(size*sizeof(int));
+    memset(count_arr, 0, (max+1)*sizeof(int));
+    memset(count_arr, 0, (size)*sizeof(int));
+  
+    for(i=0;i<size;++i)
+     {
+      count_arr[arr[i]]++;
+     }
+   
+    for(i=1;i<=max;++i)
+     {
+      count_arr[i]+=count_arr[i-1];
+     }    
+
+    for(i=size-1; i>=0; --i)
+     { 
+       output_arr[count_arr[arr[i]]-1] = arr[i];
+       count_arr[arr[i]]--;
+       
+     }
+     
+
+    for(i=0;i<size;++i)
       {
-         key=arr[i];
-         j=i-1;
-
-         while(j>=0 && arr[j]>key)
-          {
-            arr[j+1] = arr[j];
-            --j;
-          }
-
-         arr[j+1]=key; 
-
+        arr[i] = output_arr[i];
+       
       }
+   for(i=0;i<size;i++)
+     printf("%d ",arr[i]); 
+
+   free(count_arr);
+   free(output_arr);
 
  }
 
