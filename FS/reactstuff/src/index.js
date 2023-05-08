7c6adc7e73d9b92ffbeed771aa9691c1b078557e
ShanksCodes <99173788+ShanksCodes@users.noreply.github.com>
2023-05-08 17:05:25 +0530

    z

--- FS/reactstuff/src/index.js
+++ FS/reactstuff/src/index.js
@@ -1,30 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+//import App from './App';
+import './login.css';
 
-function FunCompEx(props)
+
+/*function FunCompEx(props)
  {
     return(
     <div>
     <h1>This is one of the components created by {props.nm} at {props.at}</h1>       
     </div>)
  }
+*/
 
 function Login()
   {
     return(
-    <div>
+    <form>
+       
+    <h1>Abhi.com</h1>
+    <input placeholder="Enter Your Name" type="text" name="name" id="name" />
+    <input
+    placeholder="Enter Your Email"
+    type="email"
+    name="email"
+    id="email"
+    />
+   
+   <input
+    placeholder="Enter Your Password"
+    type="password"
+    name="password"
+    id="password"
+  />
+
+   <input type="submit" value="Login" />
     
  
-    </div>)
+    </form>)
   }
  
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-    <FunCompEx nm="abhi" at="bciiit"/>
-    <Login/>
+
+    <Login />
   </React.StrictMode>
 );
