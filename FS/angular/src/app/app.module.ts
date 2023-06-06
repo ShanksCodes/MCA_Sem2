736c1261fc22be6af37ad75c7a5d7f27815f5335
ShanksCodes <99173788+ShanksCodes@users.noreply.github.com>
2023-06-06 11:56:12 +0530

    z

---
+++ FS/angular/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SchoolComponent } from './school/school.component';
+import { StudentsComponent } from './students/students.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SchoolComponent,
+    StudentsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ FS/angular/src/app/school/school.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SchoolComponent } from './school.component';
+
+describe('SchoolComponent', () => {
+  let component: SchoolComponent;
+  let fixture: ComponentFixture<SchoolComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [SchoolComponent]
+    });
+    fixture = TestBed.createComponent(SchoolComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
